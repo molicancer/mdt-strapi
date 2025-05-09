85d439fcfce06a7d20c05756edf12385705414bc
James <molicancer@gmail.com>
2025-05-09 09:32:26 +0000

    更新package.json以添加测试上传脚本，修改中间件配置以支持内容安全策略和大文件上传，更新插件配置以允许上传最大100MB的文件。

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,31 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ['*'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '100mb',
+      jsonLimit: '100mb',
+      textLimit: '100mb',
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024,
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,18 @@
-export default () => ({
-  // 此处可以添加其他插件配置
-});
+export default {
+  // 配置上传插件允许大文件上传
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {},
+      sizeLimit: 100 * 1024 * 1024, // 设置为100MB
+      breakpoints: {
+        xxlarge: 2400,
+        xlarge: 1920,
+        large: 1080,
+        medium: 720,
+        small: 520,
+        xsmall: 320,
+      },
+    },
+  },
+};
